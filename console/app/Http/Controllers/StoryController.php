c3d25bbba6d2546931cce3b811b6f64de9c51c89
Maikls <maikls.zalkalns@gmail.com>
2023-08-28 14:56:47 +0300

    Update StoryController.php

--- console/app/Http/Controllers/StoryController.php
+++ console/app/Http/Controllers/StoryController.php
@@ -6,17 +6,9 @@ use App\Http\Services\Story\Delete\DeleteStoryRequest;
 use App\Http\Services\Story\Delete\DeleteStoryService;
 use App\Http\Services\Story\Get\GetPaginatedStoryRequest;
 use App\Http\Services\Story\Get\GetPaginatedStoryService;
-use App\Models\StoryRepository;
 
 class StoryController extends Controller
 {
-    private StoryRepository $repository;
-
-    public function __construct(StoryRepository $repository)
-    {
-        $this->repository = $repository;
-    }
-
     public function delete(DeleteStoryService $service, int $storyId)
     {
         return $service->__invoke(
@@ -27,7 +19,7 @@ class StoryController extends Controller
     public function getPaginatedStories(GetPaginatedStoryService $service, $page, $perPage): \Illuminate\Http\JsonResponse
     {
         return $service->__invoke(
-            new GetPaginatedStoryRequest($page,$perPage)
+            new GetPaginatedStoryRequest($page, $perPage)
         );
     }
 }
